d8cd3123dc12dff6d5294ec3294565a7d25ed285
Flambé <emilycogsdill@gmail.com>
2024-11-30 11:08:55 -0700

    chessboards are checkered lol

--- frontend/src/components/Chessboard.js
+++ frontend/src/components/Chessboard.js
@@ -5,9 +5,14 @@ import "../styles/Chessboard.css";
 const Chessboard = ({ chessboard }) => {
   // Render a single square of the chessboard
   const renderSquare = (row, col) => {
+    const isDark = (row + col) % 2 === 1; // Alternate colors
     const piece = chessboard[row][col];
+
     return (
-      <div key={`${row}-${col}`} className="square">
+      <div
+        key={`${row}-${col}`}
+        className={`square ${isDark ? "dark" : "light"}`}
+      >
         {piece && <Piece type={piece.type} color={piece.color} />}
       </div>
     );
@@ -15,12 +20,14 @@ const Chessboard = ({ chessboard }) => {
 
   // Render the entire chessboard
   return (
-    <div className="chessboard">
-      {chessboard.map((row, rowIndex) => (
-        <div key={rowIndex} className="row">
-          {row.map((_, colIndex) => renderSquare(rowIndex, colIndex))}
-        </div>
-      ))}
+    <div className="chessboard-container">
+      <div className="chessboard">
+        {chessboard.map((row, rowIndex) => (
+          <div key={rowIndex} className="row">
+            {row.map((_, colIndex) => renderSquare(rowIndex, colIndex))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
