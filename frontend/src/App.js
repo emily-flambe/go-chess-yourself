c2cb5339ab8f1337ad64bcaac3b27012075bd97c
Flambé <emilycogsdill@gmail.com>
2024-12-02 15:27:17 -0700

    add a second white king

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import Chessboard from "./components/Chessboard";
 
 const App = () => {
+  // Chessboard state: 8x8 grid with two Kings
   const [chessboard, setChessboard] = useState([
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
     [null, null, null, { type: "King", color: "White" }, null, null, null, null],
-    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, { type: "King", color: "White" }, null, null],
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
